c80e99ca0fd49cdfb4943f346458f207b951aaef
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-04-15 22:59:12 +0200

    Fix to pep8 as posible

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -2,9 +2,9 @@
 
 import os
 import sys
+import unittest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from constants import *
-import unittest
 
 
 class TestConstants(unittest.TestCase):
