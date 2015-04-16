7fc5d1642afe8d914395d1bedd016525e0b12ff4
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-04-16 22:02:55 +0200

    Add test oAuth.v1

--- tests/test_twitter_api_connection.py
+++ tests/test_twitter_api_connection.py
@@ -10,12 +10,32 @@ from constants import *
 
 class TestTwitterApiConnection(unittest.TestCase):
 
-    def test_obtain_first_auth(self):
+    def test_twitter_api_connection(self):
         twitter = Twython(APP_KEY, APP_SECRET)
         first_auth = twitter.get_authentication_tokens()
         self.assertNotEqual(first_auth['oauth_token'], '')
         self.assertNotEqual(first_auth['oauth_token_secret'], '')
         self.assertNotEqual(first_auth['auth_url'], '')
+        OAUTH_TOKEN = first_auth['oauth_token']
+        OAUTH_TOKEN_SECRET = first_auth['oauth_token_secret']
+        URL_SECRET = first_auth['auth_url']
+        print URL_SECRET
+        PIN_CODE = raw_input('Pin code: ')
+        twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        final_tokens = twitter.get_authorized_tokens(PIN_CODE)
+        self.assertNotEqual(final_tokens['oauth_token'], '')
+        self.assertNotEqual(final_tokens['oauth_token_secret'], '')
+        FINAL_OAUTH_TOKEN = final_tokens['oauth_token']
+        FINAL_OAUTH_TOKEN_SECRET = final_tokens['oauth_token_secret']
+        twitter = Twython(
+            APP_KEY,
+            APP_SECRET,
+            FINAL_OAUTH_TOKEN,
+            FINAL_OAUTH_TOKEN_SECRET
+        )
+        credentials = twitter.verify_credentials()
+        self.assertNotEqual(credentials['screen_name'], '')
+
 
 if __name__ == '__main__':
     unittest.main()
