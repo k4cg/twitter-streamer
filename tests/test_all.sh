817717958156cf8f922dd1ff3d94de6b47e19619
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-04-17 10:05:31 +0200

    Add PEP8 check to tests

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Test PEP8
+pep8 ../constants.py
+pep8 ../globals.py
+pep8 ../__init__.py
+pep8 ../main.py
+pep8 test_constants.py
+pep8 test_my_twython_streamer.py
+pep8 test_twitter_api_connection.py
+
 chmod ug+x *
 ./test_constants.py
 ./test_twitter_api_connection.py
