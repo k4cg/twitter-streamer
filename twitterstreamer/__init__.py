a503ca50c5e8e1e3cd57fae78e72b493fc0179ff
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-08-27 14:13:34 +0200

    Make a clean new structure: only one function to get tokens, getAuthTokens()

--- twitterstreamer/__init__.py
+++ twitterstreamer/__init__.py
@@ -2,43 +2,26 @@ from twython import Twython
 import config
 
 
-def getAuthPin():
-    f_firstAuth = open('.tmp_firstAuth', 'r')
-    f_authPin = open('.tmp_authPin', 'w')
-
-    # check if first auth was receaved
-    firstAuth = {}
-    for line in f_firstAuth:
-      key, value = line.split('\t')
-      firstAuth[key] = value
-
-    # if first auth not receaved, get new one
-    if 'auth_url' in firstAuth.keys() and firstAuth['auth_url'] == '':
-      firstAuth = getFirstAuth()
-
-    # get auth pin
-    print 'Go to this link and allow twitter access: ' + firstAuth['auth_url']
-    authPin = raw_input('Enter auth pin: ')
-
-    # and save it to a file
-    f_authPin.write(authPin)
-
-    f_firstAuth.close()
-    f_authPin.close()
-    return authPin
-
-
-def getFirstAuth():
-    f_firstAuth = open('.tmp_firstAuth', 'w')
+def getAuthTokens():
+    f_authTokens = open('.tmp_tokens', 'w')
 
     # get first auth
     twitter = Twython(config.APP_KEY, config.APP_SECRET)
     firstAuth = twitter.get_authentication_tokens()
 
-    # and save it to a file
-    for key in firstAuth:
-      line = key + '\t' + firstAuth[key] + '\n'
-      f_firstAuth.write(line)
+    # get auth pin
+    print 'auth url: ' + firstAuth['auth_url']
+    authPin = raw_input('pin: ')
 
-    f_firstAuth.close()
-    return firstAuth
+    # get second auth
+    twitter = Twython(config.APP_KEY, config.APP_SECRET, firstAuth['oauth_token'], firstAuth['oauth_token_secret'])
+    secondAuth = twitter.get_authorized_tokens(authPin)
+
+    # save tokens to a file
+    for key in secondAuth:
+        line = key + '\t' + secondAuth[key] + '\n'
+        f_authTokens.write(line)
+
+    f_authTokens.close()
+
+    return secondAuth
