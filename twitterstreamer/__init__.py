ca0448387339f5efbcd8cc6cb6b633244afdd5e0
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-08-27 12:13:11 +0200

    Make a clean new structure: Add getAuthPin() function

--- twitterstreamer/__init__.py
+++ twitterstreamer/__init__.py
@@ -2,6 +2,32 @@ from twython import Twython
 import config
 
 
+def getAuthPin():
+    f_firstAuth = open('.tmp_firstAuth', 'r')
+    f_authPin = open('.tmp_authPin', 'w')
+
+    # check if first auth was receaved
+    firstAuth = {}
+    for line in f_firstAuth:
+      key, value = line.split('\t')
+      firstAuth[key] = value
+
+    # if first auth not receaved, get new one
+    if 'auth_url' in firstAuth.keys() and firstAuth['auth_url'] == '':
+      firstAuth = getFirstAuth()
+
+    # get auth pin
+    print 'Go to this link and allow twitter access: ' + firstAuth['auth_url']
+    authPin = raw_input('Enter auth pin: ')
+
+    # and save it to a file
+    f_authPin.write(authPin)
+
+    f_firstAuth.close()
+    f_authPin.close()
+    return authPin
+
+
 def getFirstAuth():
     f_firstAuth = open('.tmp_firstAuth', 'w')
 
