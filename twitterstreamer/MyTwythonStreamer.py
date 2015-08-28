92a4abb2cb476e8c4b670544d5fcd269b76df5cd
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-08-28 11:31:01 +0200

    add log activity

--- twitterstreamer/MyTwythonStreamer.py
+++ twitterstreamer/MyTwythonStreamer.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from twython import TwythonStreamer
 import config
@@ -7,14 +8,23 @@ class MyTwythonStreamer(TwythonStreamer):
 
     def on_error(self, status_code, data):
         print status_code, data
-        # Requires Authentication as of Twitter API v1.1
 
     def on_success(self, data):
         if 'text' in data:
+            # get json object notation string
             json_string_data = json.dumps(data)
+
+            # line to save into a file
             line = json_string_data + '\n'
+
+            # save to a file
             f_trackedTweets = open(config.FILE_TRACKED_TWEETS, 'a')
             f_trackedTweets.write(line)
             f_trackedTweets.close()
-            # Want to disconnect after the first result?
-            # self.disconnect()
+
+            # show log info
+            log_datetime = datetime.datetime.today()
+            log_message = ' Message receaved and saved. '
+            log_message += 'File: ' + config.FILE_TRACKED_TWEETS
+            logline = '[' + str(log_datetime) + '] ' + log_message
+            print logline
